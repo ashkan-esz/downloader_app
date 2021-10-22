41a674d60b2ae96df99e9d6428638972c08c6541
ashkan <ashkanaz2828@gmail.com>
2021-10-23 01:02:10 +0330

    Feature: add splash screen
    Feature: add splash screen 2
    
    Feature: add splash screen

--- app.config.js
+++ app.config.js
@@ -1,14 +1,5 @@
 export default {
-    // icon:
-    //     "https://github.com/expo/expo/blob/master/templates/expo-template-blank/assets/icon.png?raw=true",
-    // splash: {
-    //     image:
-    //         "https://github.com/expo/expo/blob/master/templates/expo-template-blank/assets/splash.png?raw=true",
-    //     resizeMode: "contain",
-    //     backgroundColor: "#ffffff",
-    // },
     "name": 'downloader_app',
-    // "icon": "./src/assets/icons/downloader_app_icon",
     "ios": {
         "bundleIdentifier": "com.ashkanaz2828.downloader-app",
         "buildNumber": "1.0.0"
