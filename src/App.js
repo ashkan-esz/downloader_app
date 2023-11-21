f521722538894cf611f7a0f01c84c12581369269
ashkan <ashkanaz2828@gmail.com>
2023-11-21 22:07:22 +0330

    chore: disable preloading images in splashScreen

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React, {useCallback, useEffect, useState} from 'react';
 import {AppState, I18nManager, View, StyleSheet, Platform} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {enableFreeze} from 'react-native-screens';
-import * as SplashScreen from 'expo-splash-screen';
-import {Asset} from 'expo-asset';
+// import * as SplashScreen from 'expo-splash-screen';
+// import {Asset} from 'expo-asset';
 import {AuthNavigations, AppStackNavigations} from "./navigation";
 import {StatusBar} from 'expo-status-bar';
 import {RootToast} from './components/atoms';
@@ -75,21 +75,21 @@ onlineManager.setEventListener(setOnline => {
 });
 
 // Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync().catch(() => {
-    /* reloading the app might trigger some race conditions, ignore them */
-});
+// SplashScreen.preventAutoHideAsync().catch(() => {
+//     /* reloading the app might trigger some race conditions, ignore them */
+// });
 
 export default function App() {
     useKeepAwake();
     const dispatch = useDispatch();
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-    const [appIsReady, setAppIsReady] = useState(false);
+    // const [appIsReady, setAppIsReady] = useState(false);
 
-    useEffect(() => {
-        if (isLoggedIn) {
-            dispatch(profile_api());
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (isLoggedIn) {
+    //         dispatch(profile_api());
+    //     }
+    // }, []);
 
     function onAppStateChange(status) {
         if (Platform.OS !== 'web') {
@@ -102,56 +102,63 @@ export default function App() {
         return () => subscription.remove()
     }, []);
 
+    // useEffect(() => {
+    //     async function prepare() {
+    //         try {
+    //             const imageAssets = [
+    //                 require('./assets/icons/logo.png'),
+    //                 require('./assets/icons/imdb.png'),
+    //                 require('./assets/icons/mal.png'),
+    //             ];
+    //             const imageAssetsPromise = imageAssets.map(image => {
+    //                 if (typeof image === 'string') {
+    //                     return Image.prefetch(image);
+    //                 } else {
+    //                     return Asset.fromModule(image).downloadAsync();
+    //                 }
+    //             });
+    //             await Promise.all(imageAssetsPromise);
+    //         } catch (error) {
+    //             console.warn(error);
+    //         } finally {
+    //             // Tell the application to render
+    //             setAppIsReady(true);
+    //         }
+    //     }
+    //
+    //     prepare();
+    // }, []);
+
+    // const onLayoutRootView = useCallback(async () => {
+    //     if (appIsReady) {
+    //         // This tells the splash screen to hide immediately! If we call this after
+    //         // `setAppIsReady`, then we may see a blank screen while the app is
+    //         // loading its initial state and rendering its first pixels. So instead,
+    //         // we hide the splash screen once we know the root view has already
+    //         // performed layout.
+    //         await SplashScreen.hideAsync();
+    //     }
+    // }, [appIsReady]);
+
     useEffect(() => {
-        async function prepare() {
-            try {
-                const imageAssets = [
-                    require('./assets/icons/logo.png'),
-                    require('./assets/icons/imdb.png'),
-                    require('./assets/icons/mal.png'),
-                ];
-                const imageAssetsPromise = imageAssets.map(image => {
-                    if (typeof image === 'string') {
-                        return Image.prefetch(image);
-                    } else {
-                        return Asset.fromModule(image).downloadAsync();
-                    }
-                });
-                await Promise.all(imageAssetsPromise);
-            } catch (error) {
-                console.warn(error);
-            } finally {
-                // Tell the application to render
-                setAppIsReady(true);
-            }
-        }
-
-        prepare();
-    }, []);
-
-    const onLayoutRootView = useCallback(async () => {
-        if (appIsReady) {
-            // This tells the splash screen to hide immediately! If we call this after
-            // `setAppIsReady`, then we may see a blank screen while the app is
-            // loading its initial state and rendering its first pixels. So instead,
-            // we hide the splash screen once we know the root view has already
-            // performed layout.
-            await SplashScreen.hideAsync();
-        }
-    }, [appIsReady]);
-
-    useEffect(() => {
-        if (isLoggedIn && appIsReady) {
+        if (isLoggedIn) {
             dispatch(profile_api());
         }
     }, [isLoggedIn]);
 
-    if (!appIsReady) {
-        return null;
-    }
+    // useEffect(() => {
+    //     if (isLoggedIn && appIsReady) {
+    //         dispatch(profile_api());
+    //     }
+    // }, [isLoggedIn]);
+    //
+    // if (!appIsReady) {
+    //     return null;
+    // }
 
     return (
-        <View style={style.container} onLayout={onLayoutRootView}>
+        // <View style={style.container} onLayout={onLayoutRootView}>
+        <View style={style.container}>
             <StatusBar style="light"
                 // barStyle={isDarkMode ? 'light-content' : 'dark-content'}
                 // backgroundColor={backgroundStyle.backgroundColor}
