c22213ad6ed20926881cf9d7140eca1245aab6e0
ashkan <ashkanaz2828@gmail.com>
2024-05-08 15:07:04 +0330

    feat: update dependencies, update expo sdk 50

--- app.config.js
+++ app.config.js
@@ -1,4 +1,18 @@
 export default {
+    "slug": "downloader_app",
+    "runtimeVersion": "1.0.0",
+    "updates": {
+        "enabled": false,
+        "fallbackToCacheTimeout": 0
+    },
+    "splash": {
+        "image": "./src/assets/images/splashScreen.png",
+        "backgroundColor": "#22222B",
+        "resizeMode": "contain"
+    },
+    "assetBundlePatterns": [
+        "assets/images/*"
+    ],
     "name": 'downloader_app',
     "ios": {
         "bundleIdentifier": "com.ashkanaz2828.downloader-app",
@@ -13,6 +27,7 @@ export default {
     "plugins": [
         "expo-av",
         "expo-splash-screen",
-        "expo-file-system"
+        "expo-file-system",
+        "expo-secure-store",
     ]
 };
